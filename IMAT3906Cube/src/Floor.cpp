e544e0256bbeebc1b8b1de595f6d57124f5cc318
Morgan Gray <P2529401@my365.dmu.ac.uk>
2021-11-08 14:04:23 +0000

    Conor Fixed texture issue

--- IMAT3906Cube/src/Floor.cpp
+++ IMAT3906Cube/src/Floor.cpp
@@ -35,6 +35,6 @@ void Floor::createFloor()
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 	// uv attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 }
