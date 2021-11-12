35089ce881526519e7b4c07b5eafb9e24a8fc041
Morgan Gray <P2529401@my365.dmu.ac.uk>
2021-11-12 00:44:23 +0000

    began the parallax mapping labs, coding from first video

--- Shaders/PXmap.fs
+++ Shaders/PXmap.fs
@@ -42,9 +42,9 @@ struct spotLight
     float outerRad;
 };
 
-vec3 getPointLight(vec3 norm, vec3 viewDir, pointLight light);
+vec3 getPointLight(vec3 norm, vec3 viewDir, pointLight light, vec2 texCoords);
 vec3 getDirectionalLight(vec3 norm, vec3 viewDir, vec2 texCoords);
-vec3 getSpotLight(vec3 norm, vec3 viewDir, spotLight light);
+vec3 getSpotLight(vec3 norm, vec3 viewDir, spotLight light, vec2 texCoords);
 vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir);
 vec2 SteepParallaxMapping(vec2 texCoords, vec3 viewDir);
 
@@ -89,4 +89,36 @@ void main()
 
     result = result + getDirectionalLight(norm, viewDir, texCoords);
     FragColor = vec4(result, 1.0f);
+}
+
+vec3 getDirectionalLight(vec3 normal, vec3 lightDir, vec3 viewDir, vec2 texCoords)
+{
+    vec3 ambient = texture(mat.diffuseTexture, texCoords).rgb*mat.ambient*light.ambient;
+    float dif = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = dif * (texture(mat.diffuseTexture, texCoords)).rgb*light.diffuse;
+    vec3 halfWay = normalize(lightDir + viewDir);
+    float spec = pow(max(dor(normal, halfWay), 0.0), mat.shininess);
+    vec3 specular = light.specular * spec * vec3(texture(mat.specularTexture, texCoords).r);
+    return ambient + diffuse + specular;
+}
+
+vec3 getPointLight(vec3 normal, vec3 viewDir, pointLight pLight, vec2 texCoords)
+{
+    vec3 lightDir = normalize(pLight.pos - posWS);
+    //diffuse shading
+    float diff = max(dor(normal, lightDir), 0.0);
+    //specular shading
+    vec3 halfWay = normalize(lightDir + viewDir);
+    float spec = pow(max(dor(normal, halfWay), 0.0), mat.shininess);
+    //attenutation
+    float distance = length(-lightDir, normal);
+    float attenuation = 1.0 / (pLight.kC + pLight.kL * distance + pLight.kQ * (distance*distance));
+    //combine results
+    vec3 ambient = pLight.ambient * vec3(texture(mat.diffuseTexture, texCoords));
+    vec3 diffuse = pLight.diffuse * diff * vec3(texture(mat.diffuseTexture, texCoords));
+    vec3 specular = pLight.specular * spec * vec3(texture(mat.specularTexture, texCoords).r);
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return ambient + diffuse + specular;
 }
