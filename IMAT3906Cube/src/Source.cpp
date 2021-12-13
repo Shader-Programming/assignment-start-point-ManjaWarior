01e411ca2cc24002ed3415b33a3308587c520127
Morgan Gray <P2529401@my365.dmu.ac.uk>
2021-12-13 18:19:46 +0000

    blur line fixed, testing with bloom

--- IMAT3906Cube/src/Source.cpp
+++ IMAT3906Cube/src/Source.cpp
@@ -241,7 +241,7 @@ void updatePerFrameUniforms(Shader& cubeShader, Shader& floorShader, Camera came
 
 void setUniforms(Shader& cubeShader, Shader& floorShader)
 {
-	float bloomMinBrightness = 0.85f;
+	float bloomMinBrightness = 0.5f;
 	cubeShader.use();
 	//directional light
 	glm::vec3 lightDirection = glm::vec3(0, -1, 0);
